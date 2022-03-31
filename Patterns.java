b9c3b821b90b3a30cc0333ad3db8bcba7231c810
naikrahul175 <naikrahul175@gmail.com>
2022-03-31 19:42:06 +0530

    Most of the usefull pattern codes to try and learn the logic

---
+++ Patterns.java
@@ -0,0 +1,98 @@
+
+public class Patterns {
+    public static void main(String args[]) { 
+        int n = 4;
+        int m = 5;
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+                 System.out.println();  // space for next pattern
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if( i == 1 || i == n || j == 1  || j == m) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }    
+            }
+            System.out.println();
+        }
+
+        System.out.println(); // space for next pattern
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+         System.out.println();
+        }
+
+        System.out.println(); // space for next pattern
+        
+        for(int i=n; i>=1; i--) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+        System.out.println(); 
+        }
+
+        System.out.println(); // space for next pattern
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+
+        System.out.println(); //space for next pattern
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(j+" ");
+            }
+        System.out.println();
+        }
+
+        int count =0;
+        System.out.println(); // space foe next  pattern
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(count++ +" ");
+            }
+        System.out.println();
+        }
+
+        System.out.println(); // space for next pattern
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                int sum = i+j;
+                if(sum % 2 == 0) {
+                    System.out.print("1 ");
+                }else {
+                    System.out.print("0 ");
+                }   
+            }
+            System.out.println();
+        }
+
+        System.out.println(); //space for next pattern
+        int counter =1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(counter +" ");
+                counter = counter + counter;
+            }
+            System.out.println();
+        }
+    }
+}
